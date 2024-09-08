a5fe1075b7c68390c52d90df297361a7c91fb29d
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-09-08 11:27:32 +0600

    add source-map

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         filename: "[name][contenthash].js"
     },
+    devtool: "source-map",
     devServer: {
         static: {
             directory: path.resolve(__dirname, 'public'),
